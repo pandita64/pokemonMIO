7e116fc92b308e33c811c3e3016fa79240b5d5ed
pandita64 <163213265+pandita64@users.noreply.github.com>
2024-06-05 22:08:57 -0500

    modificacion botones

--- src/services/PokemonServices.js
+++ src/services/PokemonServices.js
@@ -3,38 +3,68 @@ import axios from "axios";
 export default {
   async getPokemons() {
     try {
-      const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=10000"); // Limit incrementado para obtener más Pokémon
       return response.data.results;
     } catch (error) {
       console.log(error);
+      throw error;
     }
   },
+  
   async getPokemonDetail(name) {
     try {
       const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-      const types = response.data.types.map((type) => type.type.name); // Obtener el tipo de pokemon
-      return { ...response.data, types }; // Retornar los detalles del Pokémon junto con los tipos
+      const types = response.data.types.map((type) => type.type.name); 
+      return { ...response.data, types };
     } catch (error) {
       console.log(error);
+      throw error;
     }
   },
+  
   async getPokemonAbilities(pokemon) {
     try {
-      const abilitiesUrls = pokemon.abilities.map((ability) => ability.url);
-      const responses = await Promise.all(abilitiesUrls.map((url) => axios.get(url))); //promesa que se resuelve con un array de habilidades del pokemon
-      return responses.map((response) => response.data); // Retorna las habilidades del pokemon
+      const abilitiesUrls = pokemon.abilities.map((ability) => ability.ability.url);
+      const responses = await Promise.all(abilitiesUrls.map((url) => axios.get(url))); 
+      return responses.map((response) => response.data);
     } catch (error) {
       console.log(error);
+      throw error;
     }
   },
-    async getRandomPokemon() {
-        const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
-        const randomIndex = Math.floor(Math.random() * 625) +1;
-        const randomPokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomIndex}`);
-        return randomPokemon.data;
-    },
-    async getPokemonImage(name) {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-        return response.data.sprites.front_default;
-    },
+  
+  async getRandomPokemon() {
+    try {
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=200");  // Limit incrementado para obtener más Pokémon
+      const randomIndex = Math.floor(Math.random() * response.data.results.length);
+      const randomPokemon = await axios.get(response.data.results[randomIndex].url);
+      return randomPokemon.data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  },
+  
+  async getPokemonImage(name) {
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      return response.data.sprites.front_default;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  },
+  async getPokemonList(start, end) {
+    let gottenPokemons = [];
+    for (let i = start; i <= end; i++) {
+      const itemPokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
+      const addedPokemon = {
+        id: itemPokemon.data.id,
+        name: itemPokemon.data.name,
+        image: itemPokemon.data.sprites.other.dream_world.front_default
+      };
+      gottenPokemons.push(addedPokemon);
+    }
+    return gottenPokemons;
+  }
 };
--- src/views/PokemonsListView.vue
+++ src/views/PokemonsListView.vue
@@ -8,9 +8,9 @@
     <div class="row p-3 mt-3 mb-3 d-flex justify-content-center">
       <div v-for="pokemon in pokemons" :key="pokemon.name" class="col-sm-10">
         <router-link :to="{ name: 'Pokemon', params: { name: pokemon.name } }">
-          <div class="card d-flex justify-content-center aling-items-center mb-3">
+          <div class="card d-flex justify-content-center align-items-center mb-3">
             <div class="card-title">
-              <img :src="pokemon.sprites?.front_default" alt="Front sprite" style="width: 180px; height: 180px;">
+              <img :src="pokemon.image" alt="Front sprite" style="width: 180px; height: 180px;">
               <h4>
                 {{ pokemon.name }}
               </h4>
@@ -19,11 +19,11 @@
         </router-link>
       </div>
     </div>
-
-    <div v-if="selectedPokemon">
-      <h2>{{ selectedPokemon.name }}</h2>
-      <img :src="selectedPokemon.sprites?.front_default" alt="Front sprite">
-      <!-- Otros detalles del Pokémon -->
+    <div class="row">
+      <div class="col d-flex justify-content-between">
+        <button @click="previousPokemons" :disabled="start <= 1" class="btn btn-primary">Anterior</button>
+        <button @click="nextPokemons" :disabled="end >= 465" class="btn btn-primary ml-3">Siguiente</button>
+      </div>
     </div>
   </div>
 </template>
@@ -38,15 +38,12 @@ export default {
       pokemons: [],
       selectedPokemonName: null,
       selectedPokemon: null,
+      start: 1,
+      end: 10
     }
   },
   async created() {
-    this.pokemons = await PokemonServices.getPokemons()
-    this.pokemons.forEach(pokemon => {
-      PokemonServices.getPokemonDetail(pokemon.name).then(res => {
-        pokemon.sprites = res.sprites
-      })
-    })
+    this.pokemons = await PokemonServices.getPokemonList(this.start, this.end);
   },
 
   watch: {
@@ -58,10 +55,23 @@ export default {
   },
 
   methods: {
-    async loadPokemonDetail(name) {
-      this.selectedPokemon = await PokemonServices.getPokemonDetail(this.selectedPokemonName)
+    async chargePokemons() {
+      this.pokemons = await PokemonServices.getPokemonList(this.start, this.end);
+    },
+    previousPokemons() {
+      if (this.start >= 1) {
+        this.start -= 10;
+        this.end -= 10;
+        this.chargePokemons();
+      }
+    },
+    nextPokemons() {
+      if (this.end <= 465) {
+        this.start += 10;
+        this.end += 10;
+        this.chargePokemons();
+      }
     }
-
   },
 
   computed: {
@@ -78,3 +88,4 @@ export default {
   }
 }
 </script>
+
